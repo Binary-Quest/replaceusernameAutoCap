28411965f5c2d7b17faea3eb08674e7f3a84f675
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-24 13:57:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ start_message = """
 <b>👋Hello {}</b>
 <b>I am an AutoCaption bot</b>
 <b>All you have to do is add me to your channel and I will show you my power</b>
-<b>@kwicbotupdates</b>"""
+<b>@MS_Update_Channel</b>"""
 
 about_message = """
-<b>• Name : <a href=https://t.me/Film_Nest>kwic autocaption</a></b>
+<b>• Name : <a href=https://t.me/MS_Samad>autocaption</a></b>
 <b>• Developer : <a href=https://t.me/Anmol0700>[ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ!]</a></b>
 <b>• Language : Python3</b>
 <b>• Library : Pyrogram v{version}</b>
-<b>• Updates : <a href=https://t.me/Film_Nest>Click Here</a></b>
-<b>• Source Code : <a href=https://t.me/Film_Nest>Click Here</a></b>"""
+<b>• Updates : <a href=https://t.me/MS_Update_Channel>Click Here</a></b>
+<b>• Source Code : <a href=https://t.me/Anmol0700>Click Here</a></b>"""
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
@@ -103,7 +103,7 @@ def get_file_details(update: pyrogram.types.Message):
 def start_buttons(bot, update):
     bot = bot.get_me()
     buttons = [[
-        pyrogram.types.InlineKeyboardButton("Updates", url="t.me/Film_Nest"),
+        pyrogram.types.InlineKeyboardButton("Updates", url="t.me/MS_Update_Channel"),
         pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
     ], [
         pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
